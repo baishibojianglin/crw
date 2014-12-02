e5df8f47eaf4a25a5684ebad0f4147fba8e335cc
linbin <git@github.com:baishibojianglin/crw.git>
2014-12-02 15:34:18 +0800

    libin

---
+++ js/libin.js
@@ -0,0 +1,31 @@
+<script>
+$right1=$("#RightArr");	
+$left1=$("#LeftArr");
+
+$window1=$(".dao-1b1c");
+	var lc1 = 0;
+	var rc1 = 3;
+	$left1.click(function(){
+	
+		if (lc1 < 1) {
+			alert("ӑޭˇسҟخ۳һҳ");
+			return;
+		}
+		lc1--;
+		rc1++;
+		$window1.animate({left:'+=287px'}, 3000);
+		
+		
+	});
+	
+	
+	$right1.click(function(){
+		if (rc1 < 1){
+			alert("ӑޭˇԒҟخ۳һҳ");
+			return;
+		}
+		lc1++;
+		rc1--;
+		$window1.animate({left:'-=287px'}, 3000);
+	});
+</script>
