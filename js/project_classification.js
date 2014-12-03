65490b62271383d73faa9eab88803b026dfa507f
jianglin <904880305@qq.com>
2014-12-03 19:26:36 +0800

    jl xiangmufenlei

--- js/project_classification.js
+++ js/project_classification.js
@@ -136,4 +136,119 @@ function top_change_background(a)
 		  top_change_background(5);
 		});	
 		
+		
+		
+		
+		
+		
+		
+		
+    //正文第一部分图片滑动特效
+$right1=$("#right");	
+$left1=$("#left");
+$window1=$(".xmfl_1div1zong");
+	var lc1 = 0;
+	var rc1 = 3;
+
+	
+function leftmove()
+{
+if (lc1 < 1)
+ {
+			alert("已经是左边最后一页");
+			return;
+ }
+		lc1--;
+		rc1++;
+		$window1.animate({left:'+=100%'}, 1000);
+		yuandian(lc1);
+		
+}
+
+
+
+function rightmove()
+{
+	
+		if (rc1 < 1){
+			alert("已经是右边最后一页");
+			return;
+		}
+		lc1++;
+		rc1--;
+		$window1.animate({left:'-=100%'}, 1000);
+		yuandian(lc1);
+		
+	
+}
+
+
+	
+$left1.click(function(){
+		
+		leftmove();	
+	
+	});
+
+$right1.click(function(){
+     
+	 rightmove();
+
+	});
+	
+     //圆点点击特效
+	 
+function c1_dianji(a)
+{
+	var x,y;
+	x=lc1-a;
+	if(x>=0)
+	{
+	for(i=0;i<x;i++)
+	$window1.animate({left:'+=100%'},5);
+	lc1=a;
+	rc1=3-lc1;
+	}
+	else
+	{
+	y=Math.abs(x);
+	for(i=0;i<y;i++)
+	$window1.animate({left:'-=100%'},5);
+	lc1=a;
+	rc1=3-lc1;
+	}
+}
+
+//原点背景初始化
+
+$(".xmfl_1divyuandian_1").eq(0).css({"background-image":"url(./images/xm1a.png)"});
+
+function yuandian(a)
+{
+   $(".xmfl_1divyuandian_1").css({"background-image":"url(./images/xm1b.png)"});	
+   $(".xmfl_1divyuandian_1").eq(a).css({"background-image":"url(./images/xm1a.png)"});	
+}
+
+	 
+$(".xmfl_1divyuandian_1").eq(0).click(function(){
+	c1_dianji(0);
+	yuandian(0);
+	});
+
+$(".xmfl_1divyuandian_1").eq(1).click(function(){
+	c1_dianji(1);
+	yuandian(1);
+
+	});
+$(".xmfl_1divyuandian_1").eq(2).click(function(){
+	c1_dianji(2);
+	yuandian(2);
+	});
+	
+$(".xmfl_1divyuandian_1").eq(3).click(function(){
+	c1_dianji(3);
+	yuandian(3);
+	});
+
+		
 });
