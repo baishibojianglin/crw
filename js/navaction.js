a9484976deae6d6d0fcefc4e90c657bce2c24c9c
linbin <git@github.com:baishibojianglin/crw.git>
2014-12-08 17:12:09 +0800

    lb

--- js/navaction.js
+++ js/navaction.js
@@ -1,7 +1,38 @@
-$(".nav a").each(function(){
-        if($(this).attr('href') == location.href){
-                $(this).css('background-color','#f00');
 
-        }
+<script>
+$(document).ready(function(){
+  $(".jie-pa7c3 img").click(function(){
+    $(".jie-pa7c5a").slideDown("slow");
+  });
+});
+$(document).ready(function(){
+  $("#xx").click(function(){
+    $(".jie-pa7c5a").slideUp("slow");
+  });
+ }); 
 
-})
+ $(document).ready(function(){
+  $(".jie-pa7c3 img").click(function(){
+    $(".jie-pa7c5a").slideDown("slow");
+  });
+});
+$(document).ready(function(){
+  $("#xx").click(function(){
+    $(".jie-pa7c5a").slideUp("slow");
+  });
+ });
+
+
+
+$(document).ready(function(){
+  $(".jie-pa81ww").mouseover(function(){
+  $(".jie-pa81").hide();
+  });
+  $(".jie-pa81ww").mouseover(function(){
+  $(".jie-pa81").show();
+  });
+});
+
+
+
+</script>
